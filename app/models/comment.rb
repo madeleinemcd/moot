f755d7e777d6fda29ca0fe25e27b113215b73643
Madeleine McDonald <mpmstarling@gmail.com>
2023-02-17 14:09:27 +0100

    fixed the friendly id show page bug

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :review
+  validates :content, presence: true
+  validates :c_nickname, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,6 @@
 class Review < ApplicationRecord
-  extend FriendlyId
-  friendly_id :title, use: :slugged
-
   belongs_to :user
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: {maximum: 300}
   validates :subtitle, presence: true
   validates :category, presence: true
@@ -10,4 +8,7 @@ class Review < ApplicationRecord
   scope :recent, -> { order(created_at: :desc) }
   scope :last_three, -> { order(created_at: :desc).limit(6) }
   # has_one_attached :photo
+
+  extend FriendlyId
+  friendly_id :title, use: :slugged
 end
